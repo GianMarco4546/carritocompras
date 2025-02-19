f5792fa9694946c771fd739aa7c6892e04f1d791
Gian Castañeda <2201080059@undc.edu.pe>
2025-02-19 08:52:44 -0500

    Add files via upload

---
+++ javascript-Totales_carrito/js/comida.js
@@ -0,0 +1,188 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const comida = [
+    {
+      id:1,
+      nombre: "Triceratops",
+      precio: 12,
+    },
+    {
+      id:2,
+      nombre: "Carnotaurus",
+      precio: 14,
+    },
+    {
+      id:3,
+      nombre: "Parasaurolophus",
+      precio: 13,
+    },
+    {
+      id:4,
+      nombre: "Saltasaurus",
+      precio: 12,
+    },
+    {
+      id:5,
+      nombre: "Carcharodontosaurus",
+      precio: 13,
+    },
+    {
+      id:6,
+      nombre: "Maiasaura",
+      precio: 13,
+    },
+    {
+      id:7,
+      nombre: "Utahraptor",
+      precio: 12,
+    },
+    {
+      id:8,
+      nombre: "Ankylosaurus",
+      precio: 14,
+    },
+    {
+      id:9,
+      nombre: "Suchomimus",
+      precio: 13,
+    },
+    {
+      id:10,
+      nombre: "Ceratosaurus",
+      precio: 12,
+    },
+    {
+      id:11,
+      nombre: "Styracosaurus",
+      precio: 14,
+    },
+    {
+      id:12,
+      nombre: "Pachycephalosaurus",
+      precio: 13,
+    },
+    {
+      id:13,
+      nombre: "Acrocanthosaurus",
+      precio: 12,
+    },
+    {
+      id:14,
+      nombre: "Daspletosaurus",
+      precio: 14,
+    },
+    {
+      id:15,
+      nombre: "Altirhinus",
+      precio: 12,
+    },
+    {
+      id:16,
+      nombre: "Euoplocephalus",
+      precio: 12,
+    },
+    {
+      id:17,
+      nombre: "Megaraptor",
+      precio: 14,
+    },
+    {
+      id:18,
+      nombre: "Amargasaurus",
+      precio: 13,
+    },
+    {
+      id:19,
+      nombre: "Anchiceratops",
+      precio: 12,
+    },
+    {
+      id:20,
+      nombre: "Therizinosaurus",
+      precio: 13,
+    },
+    {
+      id:21,
+      nombre: "Torosaurus",
+      precio: 12,
+    },
+    {
+      id:22,
+      nombre: "Saurolophus",
+      precio: 14,
+    },
+    {
+      id:23,
+      nombre: "Baryonyx",
+      precio: 13,
+    },
+    {
+      id:24,
+      nombre: "Deltadromeus",
+      precio: 12,
+    },
+    {
+      id:25,
+      nombre: "Deinonychus",
+      precio: 14,
+    },
+    {
+      id:26,
+      nombre: "Iguanodon",
+      precio: 13,
+    },
+    {
+      id:27,
+      nombre: "Mapusaurus",
+      precio: 12,
+    },
+    {
+      id:28,
+      nombre: "Allosaurus",
+      precio: 14,
+    },
+    {
+      id:29,
+      nombre: "Pentaceratops",
+      precio: 12,
+    },
+    {
+      id:30,
+      nombre: "Fukuisaurus",
+      precio: 14,
+    },
+    {
+      id:31,
+      nombre: "Chocolate",
+      precio: 13,
+    },
+    {
+      id:32,
+      nombre: "Tyrannosaurus",
+      precio: 12,
+    },
+    {
+      id:33,
+      nombre: "Spinosaurus",
+      precio: 14,
+    },
+    {
+      id:34,
+      nombre: "Saichania",
+      precio: 13,
+    },
+    {
+      id:35,
+      nombre: "Stegosaurus",
+      precio: 12,
+    },
+    {
+      id:36,
+      nombre: "Ampelosaurus",
+      precio: 14,
+    },
+    {
+      id:37,
+      nombre: "Saurophaganax",
+      precio: 12,
+    },
+  ]
